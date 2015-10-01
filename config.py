09ecf6a6e0b6f9d61c089bf2624f6d959a5320c9
Maxim Burda <maximzxc@gmail.com>
2015-10-01 19:07:06 +0300

    config added

---
+++ config.py
@@ -0,0 +1,14 @@
+# all data bellow is not valid it's just created to help you to
+# realize what kind of data is expected
+
+INSTANCE_TYPE = 't2.micro'
+DATA_VOLUME_SIZE = '8G'
+DATA_VOLUME_RATE = 100
+DATA_VOLUME_TYPE = 'io1'
+AWS_REGION = 'us-west-2'
+AWS_AMI_IMAGE_ID = 'ami-f53b97b6'
+AWS_KEY_NAME = 'your-aws-key-name'
+AWS_SECURITY_GROUPS = ['sr-b47f11z1']
+AWS_SBNET_ID  = 'subnet-75bc7719'
+AWS_ACCESS_KEY_ID = 'your_key_id'
+AWS_SECRET_ACCESS_KEY = 'your_secret_key'
